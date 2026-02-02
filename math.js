f0d3162b24f7dbbeca2bba8353248b68775c8234
Mehedi <mehedi.interface@gmail.com>
2026-02-02 13:05:59 +0600

    18-6 Math, abs, round, ceil, floor, and random number

---
+++ math.js
@@ -0,0 +1,15 @@
+const min = Math.min(45, 21, 65, 99, 126, 5, -99); 
+console.log(min); 
+const max = Math.max(45, 21, 65, 99, 126, 5, -99); 
+console.log(max)
+
+console.log(Math.PI); 
+console.log(Math.abs(5 - 100)); 
+console.log(Math.round(2.25)); 
+console.log(Math.floor(2.99)); 
+console.log(Math.ceil(2.99)); 
+console.log(Math.random()); 
+console.log(Math.random() * 10); 
+
+const rand = Math.round(Math.random() * 10)
+console.log(rand); 
